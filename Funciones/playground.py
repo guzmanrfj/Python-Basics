16b2e70631e78998936c1e4be8f52bc5b2af6d24
guzmanrfj <guzmanrfj@gmail.com>
2025-07-23 13:54:49 -0600

    Se agregaron varios ejemplos de funciones y de booleanos

--- Funciones/playground.py
+++ Funciones/playground.py
@@ -53,3 +53,46 @@ def sign(num):
         return(0)
 
 print(sign(0.000001))
+
+def concise_is_negative(number):
+    pass 
+    return True if number < 0 else False
+
+print(concise_is_negative(-8))
+
+def wants_all_toppings(ketchup, mustard, onion):
+    """Return whether the customer wants "the works" (all 3 toppings)
+    """
+    pass
+
+    return ketchup and mustard and onion
+
+
+def wants_plain_hotdog(ketchup, mustard, onion):
+    """Return whether the customer wants a plain hot dog with no toppings.
+    """
+    pass
+
+    return not ketchup and not mustard and not onion
+
+# One solution looks like:
+
+#return not ketchup and not mustard and not onion
+#We can also "factor out" the nots to get:
+
+#return not (ketchup or mustard or onion)
+
+
+def exactly_one_topping(ketchup, mustard, onion):
+    """Return whether the customer wants exactly one of the three available toppings
+    on their hot dog.
+    """
+    pass
+    return (int(ketchup) + int(mustard) + int(onion)) == 1
+
+#This condition would be pretty complicated to express using just and, or and not, but using boolean-to-integer conversion gives us this short solution:
+
+#return (int(ketchup) + int(mustard) + int(onion)) == 1
+#Fun fact: we don't technically need to call int on the arguments. Just by doing addition with booleans, Python implicitly does the integer conversion. So we could also write...
+
+#return (ketchup + mustard + onion) == 1
