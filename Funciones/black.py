077038be991d475dd124f13581dd95dabf443031
guzmanrfj <guzmanrfj@gmail.com>
2025-07-30 11:10:34 -0600

    Ejercicio Correcto BlackJack

--- Funciones/black.py
+++ Funciones/black.py
@@ -1,6 +1,6 @@
 
-hand_1 = ["A","5","9"]
-hand_2 = ["3","4"]
+hand_1 = ['8']
+hand_2 = ['2', 'K']
 def blackjack_hand_greater_than(hand_1, hand_2):
     """
     Return True if hand_1 beats hand_2, and False otherwise.
@@ -42,63 +42,96 @@ def blackjack_hand_greater_than(hand_1, hand_2):
         else:
             total_hand1 = total_hand1 + int(card)
     
-    if total_hand1 <= 20:
-        if cuenta_as == 1 and total_hand1 + 11 > 21:
-            total_hand1 = total_hand1 + 1
-        elif cuenta_as == 1 and total_hand1 + 11 <= 21:
-            total_hand1 = total_hand1 + 11
-        elif cuenta_as == 2 and total_hand1 + 12 > 21: 
-            total_hand1 = total_hand1 + 2
-        elif cuenta_as == 2 and total_hand1 + 12 <= 21:
-            total_hand1 = total_hand1 + 12
-        elif cuenta_as == 3 and total_hand1 + 13 > 21:
-            total_hand1 = total_hand1 + 3
-        elif cuenta_as == 3 and total_hand1 + 13 <= 21:
-            total_hand1 = total_hand1 + 13
-        elif cuenta_as == 4 and total_hand1 + 14 > 21:
-            total_hand1 = total_hand1 + 4
-        elif cuenta_as == 4 and total_hand1 + 14 <= 21:
-            total_hand1 = total_hand1 + 14 
+    if total_hand1 == 21 and cuenta_as == 0:
+        print(total_hand1)
+        print("Ganó la Mano 1")
+        return True
     else:
-        return False
-
-    total_hand1 = 0
-    cuenta_as = 0
-    for card in hand_1:
-        if card.lower() == "j":
-            total_hand1 = total_hand1 + 10
-        elif card.lower() == "q":
-            total_hand1 = total_hand1 + 10
-        elif card.lower() == "k":
-            total_hand1 = total_hand1 + 10
-        elif card.lower() == "a":
-            cuenta_as = cuenta_as + 1
+        if total_hand1 <= 20:
+            if cuenta_as == 1 and total_hand1 + 11 > 21:
+                total_hand1 = total_hand1 + 1
+            elif cuenta_as == 1 and total_hand1 + 11 <= 21:
+                total_hand1 = total_hand1 + 11
+            elif cuenta_as == 2 and total_hand1 + 12 > 21: 
+                total_hand1 = total_hand1 + 2
+            elif cuenta_as == 2 and total_hand1 + 12 <= 21:
+                total_hand1 = total_hand1 + 12
+            elif cuenta_as == 3 and total_hand1 + 13 > 21:
+                total_hand1 = total_hand1 + 3
+            elif cuenta_as == 3 and total_hand1 + 13 <= 21:
+                total_hand1 = total_hand1 + 13
+            elif cuenta_as == 4 and total_hand1 + 14 > 21:
+                total_hand1 = total_hand1 + 4
+            elif cuenta_as == 4 and total_hand1 + 14 <= 21:
+                total_hand1 = total_hand1 + 14 
         else:
-            total_hand1 = total_hand1 + int(card)
+            return False
+
+    total_hand2 = 0
+    cuenta_as2 = 0
+    for card in hand_2:
+        if card.lower() == "j":
+            total_hand2 = total_hand2 + 10
+        elif card.lower() == "q":
+            total_hand2 = total_hand2 + 10
+        elif card.lower() == "k":
+            total_hand2 = total_hand2 + 10
+        elif card.lower() == "a":
+            cuenta_as2 = cuenta_as2 + 1
+        else:
+            total_hand2 = total_hand2 + int(card)
     
-    if total_hand1 <= 20:
-        if cuenta_as == 1 and total_hand1 + 11 > 21:
-            total_hand1 = total_hand1 + 1
-        elif cuenta_as == 1 and total_hand1 + 11 <= 21:
-            total_hand1 = total_hand1 + 11
-        elif cuenta_as == 2 and total_hand1 + 12 > 21: 
-            total_hand1 = total_hand1 + 2
-        elif cuenta_as == 2 and total_hand1 + 12 <= 21:
-            total_hand1 = total_hand1 + 12
-        elif cuenta_as == 3 and total_hand1 + 13 > 21:
-            total_hand1 = total_hand1 + 3
-        elif cuenta_as == 3 and total_hand1 + 13 <= 21:
-            total_hand1 = total_hand1 + 13
-        elif cuenta_as == 4 and total_hand1 + 14 > 21:
-            total_hand1 = total_hand1 + 4
-        elif cuenta_as == 4 and total_hand1 + 14 <= 21:
-            total_hand1 = total_hand1 + 14 
+    if total_hand2 <= 20:
+        if cuenta_as2 == 1 and total_hand2 + 11 > 21:
+            total_hand2 = total_hand2 + 1
+        elif cuenta_as2 == 1 and total_hand2 + 11 <= 21:
+            total_hand2 = total_hand2 + 11
+        elif cuenta_as2 == 2 and total_hand2 + 12 > 21: 
+            total_hand2 = total_hand2 + 2
+        elif cuenta_as2 == 2 and total_hand2 + 12 <= 21:
+            total_hand2 = total_hand2 + 12
+        elif cuenta_as2 == 3 and total_hand2 + 13 > 21:
+            total_hand2 = total_hand2 + 3
+        elif cuenta_as2 == 3 and total_hand2 + 13 <= 21:
+            total_hand2 = total_hand2 + 13
+        elif cuenta_as2 == 4 and total_hand2 + 14 > 21:
+            total_hand2 = total_hand2 + 4
+        elif cuenta_as2 == 4 and total_hand2 + 14 <= 21:
+            total_hand2 = total_hand2 + 14 
     else:
-        return False    
-    print(cuenta_as)
-    print(total_hand1)  
+        return True
     
-    
-    
+    if total_hand1 == 21:
+        print(total_hand1," ",total_hand2)
+        print("Ganó la Mano 1")
+        return True
+    elif total_hand1 > 21:
+        print(total_hand1," ",total_hand2)
+        return False
+    elif total_hand1 > total_hand2 and total_hand1 <= 21:
+        print(total_hand1," ",total_hand2)
+        print("Ganó la Mano 1")
+        return True
+    elif total_hand1 < total_hand2 and total_hand2 > 21:
+        print(total_hand1," ",total_hand2)
+        print("Ganó la Mano 1")
+        return True
+    elif total_hand1 == total_hand2 and total_hand1 <= 21:
+        print(total_hand1," ",total_hand2)
+        print("Ganó la Mano 2")
+        return False 
+    elif total_hand1 < total_hand2 and total_hand2 > 21:
+        print(total_hand1," ",total_hand2)
+        print("Ganó la Mano 1")
+        return True
+    elif total_hand1 < total_hand2 and total_hand2 <=21:
+        print(total_hand1," ",total_hand2)
+        print("Ganó la Mano 2")
+        return False
+    else:
+        print(total_hand1," ",total_hand2)
+        print("Ganó la Mano 2")
+        return False
+ 
 
 blackjack_hand_greater_than(hand_1, hand_2)
