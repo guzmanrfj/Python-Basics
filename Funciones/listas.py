2bda31d4a8f5f648318668c23bcf73bd0d581cfb
guzmanrfj <guzmanrfj@gmail.com>
2025-08-04 13:55:18 -0600

    Masejecicioslistas

--- Funciones/listas.py
+++ Funciones/listas.py
@@ -47,3 +47,33 @@ def fashionably_late(arrivals, name):
 resultados = fashionably_late(arrivals, name)
 
 print(resultados)
+
+num_customers = [137, 147, 135, 128, 170, 174, 165, 146, 126, 159,
+                 141, 148, 132, 147, 168, 153, 170, 161, 148, 152,
+                 141, 151, 131, 149, 164, 163, 143, 143, 166, 171]
+
+# TODO: Fill in values for the variables below
+avg_first_seven = sum(num_customers[:7])/7
+avg_last_seven = sum(num_customers[-7:])/7 
+max_month = max(num_customers)
+min_month = min(num_customers)
+#print(num_customers[-7:])
+#print(avg_last_seven)
+#print(max_month)
+#print(min_month)
+
+def percentage_liked(ratings):
+    
+    list_liked = [i>=4 for i in ratings]
+    #print(list_liked)
+    # TODO: Complete the function
+    percentage_liked = sum(eva == True for eva in list_liked)/len(ratings)
+    
+    return percentage_liked
+    
+
+# Do not change: should return 0.5
+percentage_liked([1, 2, 3, 4, 5, 4, 5, 1])
+
+
+
